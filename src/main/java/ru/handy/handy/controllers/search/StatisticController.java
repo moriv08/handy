5e927f593726e2d42baf49c822c843f566c68482
moriv08 <moriv08@yandex.ru>
2021-03-03 20:50:52 +0300

    check statistic

--- src/main/java/ru/handy/handy/controllers/search/StatisticController.java
+++ src/main/java/ru/handy/handy/controllers/search/StatisticController.java
@@ -4,17 +4,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.handy.handy.models.PrincipalEntity;
-import ru.handy.handy.models.lead_heap.LeadEntity;
-import ru.handy.handy.models.mappers.Labels;
 import ru.handy.handy.service.PrincipalUserDetailsService;
 import ru.handy.handy.service.lead_heap.LeadService;
 import ru.handy.handy.service.service.StatisticService;
 
-import java.net.http.HttpHeaders;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
